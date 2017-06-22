2d4427e68e308b4d5f09c9fe6b30f2765a5dde75
Leon <leon.fm100@gmail.com>
2017-06-22 20:03:04 +0200

    Fixes

--- src/router/views/browse/artist/header/compact.vue
+++ src/router/views/browse/artist/header/compact.vue
@@ -1,14 +1,6 @@
 <template>
 <div class="header-container">
-<<<<<<< HEAD
   <h1 class="headline">Ellie Goulding</h1>
   <h2 class="subline"><span>Electro</span><span>Pop</span><span>Folk</span></h2>
-=======
-  <div class="header-image mobile-hidden"><img src="/static/images/header.jpg" alt="Ellie Goulding" /></div>
-  <div class="header-content">
-    <h1 class="headline">Ellie Goulding</h1>
-    <h2 class="subline transparent"><span>Electro</span><span>Pop</span><span>Folk</span></h2>
-  </div>
->>>>>>> 7bbec5d8750e111b950a04a410e833ba7b24b26d
 </div>
 </template>
--- src/router/views/browse/artist/header/full.vue
+++ src/router/views/browse/artist/header/full.vue
@@ -1,20 +1,8 @@
 <template>
 <div class="header-container">
-<<<<<<< HEAD
-  <h2 class="subline">Artist</h2>
-  <h1 class="headline">Ellie Goulding</h1>
-  <h2 class="subline"><span>Electro</span><span>Pop</span><span>Folk</span></h2>
-  <div class="header-buttons">
-    <div class="btn-combined">
-      <a class="btn btn-accent"><i class="material-icons">play_circle_filled</i>Play All</a>
-      <a class="btn"><i class="material-icons">add_circle</i>Follow</a>
-      <a class="btn btn-icon"><i class="material-icons">favorite</i></a>
-=======
-  <div class="header-image mobile-hidden"><img src="/static/images/header.jpg" alt="Ellie Goulding" /></div>
-  <div class="header-content">
+  <<<<<<< HEAD <h2 class="subline">Artist</h2>
     <h1 class="headline">Ellie Goulding</h1>
     <h2 class="subline"><span>Electro</span><span>Pop</span><span>Folk</span></h2>
-    <p class="description">Having caught the public's attention in 2010 with her singles "Starry Eyed" and "Guns and Horses," British vocalist Ellie Goulding quickly rose to the top of the charts with her infectious electronic pop sound.</p>
     <div class="header-buttons">
       <div class="btn-combined">
         <a class="btn btn-accent"><i class="material-icons">play_circle_filled</i>Play All</a>
@@ -22,9 +10,6 @@
         <a class="btn btn-icon"><i class="material-icons">favorite</i></a>
       </div>
       <a class="btn btn-transparent"><i class="material-icons">share</i>Share</a>
->>>>>>> 7bbec5d8750e111b950a04a410e833ba7b24b26d
     </div>
-    <a class="btn btn-transparent"><i class="material-icons">share</i>Share</a>
-  </div>
 </div>
 </template>
