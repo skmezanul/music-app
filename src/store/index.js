389eda125329990f4e4db76af1046c21f885cc31
Leon <leon-fm@gmx.de>
2017-07-09 16:38:49 +0200

    Setup Vuex Store
    Array of data is now loading from vuex store.

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,17 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import singles from './singles'
+import similar from './similar'
+
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
   strict: true,
   state: {
     singles,
+    similar,
+    playing: true,
+    collapsed: true
   }
 });
 
