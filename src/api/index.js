dbe5ee856c5c3803b6be7dfdafa2c994c741233a
Leon <l.seipp@browserwerk.de>
2017-07-17 10:11:10 +0200

    Added Comment

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,7 @@
 import * as SpotifyWebApi from 'spotify-web-api-js';
 
+// I know you don't expose your api keys to the public. It's only temporary for development.
+
 const spotifyApi = new SpotifyWebApi();
 
 spotifyApi.setAccessToken('BQDXYKx8j1sldLWVF8qfNaXFy3rc5fvm6EwMctYR3gjxTIyqgjdYtLo6drerUjvw38RGp_llpqrR83bK5KSsSA');
