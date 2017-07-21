03271019bb9ef19d64c8ecc51adcad2895deacf8
Leon <leon-fm@gmx.de>
2017-07-21 22:58:39 +0200

    Removed token

--- src/api/index.js
+++ src/api/index.js
@@ -4,6 +4,6 @@ import * as SpotifyWebApi from 'spotify-web-api-js';
 
 const spotifyApi = new SpotifyWebApi();
 
-spotifyApi.setAccessToken('BQCk28CBRGB6EGsKocix_-QJuE0fxJ-gKlziBiPAPoHwjhyGWxH0cDN6RyayycAWRVrfSO-_fj0P-LSDf26jOvcNapC_wmNJOCv6MKo38RXprnJ3dVkywqM87bu-oRIa7NIDexcJiBEzwYaf_OJcgNIa9PAcRxNL6UYLKIjHNj7LIJ8kjkNaQkG7E_nTPD0NsHZ9jSc_mnQFoxE');
+spotifyApi.setAccessToken('');
 
 export default spotifyApi;
