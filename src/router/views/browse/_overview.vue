bc18e08d94282947aa78f10719296ef30823879b
Leon <leon-fm@gmx.de>
2017-07-31 20:28:18 +0200

    Added Browse

---
+++ src/router/views/browse/_overview.vue
@@ -0,0 +1,15 @@
+<template>
+<div class="page-container">
+
+  <section class="page-section recommended">
+    <div class="section-header">
+      <h1>Recommended</h1>
+      <div class="section-actions">
+        <span>Show Less<i class="material-icons">keyboard_arrow_up</i></span>
+      </div>
+    </div>
+
+  </section>
+
+</div>
+</template>
---
+++ src/router/views/browse/index.vue
@@ -0,0 +1,55 @@
+<template>
+<main class="main-container" :class="{ 'stage-compact': $route.meta.stage === 'compact' }">
+
+  <!--Stage-->
+  <stage
+  type="browse"
+  :image="$store.state.currentUser.images[0].url"
+  :navigation="navigation"
+  :title="welcomeMessage"
+  ></stage>
+
+  <!--Router View-->
+  <router-view></router-view>
+
+</main>
+</template>
+
+<script>
+import spotifyApi from '../../../api/'
+
+export default {
+  data() {
+    return {
+      navigation: [{
+          title: 'Overview',
+          link: ''
+        },
+        {
+          title: 'Charts',
+          link: 'charts'
+        },
+        {
+          title: 'Genres & Moods',
+          link: 'genresandmoods'
+        },
+        {
+          title: 'New Releases',
+          link: 'newreleases'
+        },
+        {
+          title: 'Discover',
+          link: 'discover'
+        }
+      ]
+    }
+  },
+  computed: {
+    welcomeMessage() {
+      var fullName = this.$store.state.currentUser.display_name
+      var firstName = fullName.split(" ")[0]
+      return "Good evening " + firstName
+    }
+  }
+}
+</script>
