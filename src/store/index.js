3bb4f7f827833372af75ba2f7bab6c4878769934
Leon <leon-fm@gmx.de>
2017-08-03 20:13:46 +0200

    Fixes

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,27 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { createVuexLoader } from 'vuex-loading'
 
 Vue.use(Vuex)
+
+const VuexLoading = createVuexLoader({
+  // The Vuex module name, 'loading' by default.
+  moduleName: 'loading',
+  // The Vue component name, 'v-loading' by default.
+  componentName: 'v-loading',
+  // Vue component class name, 'v-loading' by default.
+  className: 'v-loading',
+});
+
+Vue.use(VuexLoading)
 
 const store = new Vuex.Store({
   strict: true,
   state: {
     artist: {},
     currentUser: {},
-    deviceID: null
+    deviceID: null,
+    loading: true
   },
   mutations: {
     artistInfo(state, response) {
@@ -20,7 +33,8 @@ const store = new Vuex.Store({
     deviceID(state, response) {
       state.deviceID = response
     }
-  }
+  },
+  plugins: [VuexLoading.Store]
 });
 
 export default store
