b878f28ae6033f9aaa9a6a715d918d4b33b28bc4
Leon <leon-fm@gmx.de>
2017-08-08 20:35:05 +0200

    Fixes

--- src/api/index.js
+++ src/api/index.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 Vue.use(VueAxios, axios);
 
 // your api token goes here:
-const token = 'BQAOMG9b_l6thynKFID9aJeNi_X7sHFXdq6OyfSUoG_HIcQRx-7KeVT8CisZEVQwPCsdrMMsVmwZ9xPxgOttWZ3neD7hQh95OMrZpsFwBnXae_84-td15FcCEG-1w-LD9zLNfQ0wnm9rgpzgFUGIPN8zVTuCZji58rb9Vaot5D_lksYAqbO5uK7aR3N06_OXBLjHTiYUn2kIWhb50Q2LIKHpIFlyJ3HHU_y91PO13kEoviqaCLQzKpZalYe3ZWqE9UCOmm8blg6IqMCPnQoX4oaU4PPI5DB0PXsy2Kh0SoFcVBqvhyS6Tdc3uAtsSxi7HgcZaQ';
+const token = '';
 // you can get it from
 // https://developer.spotify.com/web-api/console/post-next/
 // remember to grant all permissions for the app to work properly.
