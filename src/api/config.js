36f48b1662bc5e5aee6a2319b0483e10e584a78e
Leon <leon-fm@gmx.de>
2018-02-20 20:14:29 +0100

    Removed config

--- src/api/config.js
+++ src/api/config.js
@@ -10,21 +10,21 @@ export const credentials = {
   expiryTime: getters.getExpiryTime,
 
   // spotify backend api token
-  spotifyBackendToken: 'BQDxUDT9WADQou2z7s_SJmloaFfQp3QF3lB7-quiVEXFkuyrVQ6zz26REGQmIR4BfeeT5WeniGRzU4DRUzq6w1VApA2gqjKP0AEH1pBwWGR7T1HbYjxjRXcHiiJIauC-Ok-vOEU6t1L5KDYiwK3GIOjc2g0vE6BjpUiTEUzszrF1Wsxx3TTydN34Wbiu56BQSHSSIOksSXIHco_KqUC2Qi3oUwBj82QknuM7TyNBFc9-p-F2E03sJjIhIpwRI0LVoXqROpQZ3bnNcLOR1Z0',
+  spotifyBackendToken: '',
 
   // youtube api key
-  youtubeApiKey: 'AIzaSyDV-D04s-RbQDwPXxadnt2LKpuC1rPveUc',
+  youtubeApiKey: '',
 };
 
 export const firebaseConfig = {
-  apiKey: 'AIzaSyApFYAyjfvuYeCqH2os5i7A0ER9Nr6hkqw',
-  authDomain: 'music-app-e05c2.firebaseapp.com',
-  databaseURL: 'https://music-app-e05c2.firebaseio.com/',
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
 };
 
 export const baseURL = {
   spotify: 'https://api.spotify.com/v1/',
-  spotifyBackend: 'https://spclient.wg.spotify.com/open-backend-2/v1/',
+  spotifyBackend: '',
   backend: 'http://api.micro100.de:3000/music-app/v1/',
   youtube: 'https://www.googleapis.com/youtube/v3/',
 };
